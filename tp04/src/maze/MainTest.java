f5ea369044868749f545dd0eeae5f372a1f4e544
Ronan Lebas <ronan.lebas@telecom-paris.fr>
2022-12-05 09:48:47 +0100

    05/12/22 tp08

---
+++ tp04/src/maze/MainTest.java
@@ -0,0 +1,11 @@
+package maze;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Maze m = new Maze();
+		m.initFromTextFile("data/labyrinthe.maze");
+	}
+
+}
---
+++ tp04/src/maze/MazeReadingException.java
@@ -0,0 +1,13 @@
+package maze;
+
+public class MazeReadingException extends Exception{
+	private final String fileName;
+	private final int line;
+	private final String errorText;
+	public MazeReadingException(String fileName, int line, String errorText) {
+		this.fileName = fileName;
+		this.line = line;
+		this.errorText = errorText;
+		System.out.println("File : "+fileName+", line : "+line+", "+errorText);
+	}
+}
