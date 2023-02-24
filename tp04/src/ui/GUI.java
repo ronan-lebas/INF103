87a4d36495c8d30c182d45792aa258353af38ba8
Ronan Lebas <ronan.lebas@telecom-paris.fr>
2023-02-24 13:25:38 +0100

    changement javadoc

--- tp04/src/ui/GUI.java
+++ tp04/src/ui/GUI.java
@@ -20,7 +20,7 @@ public class GUI extends JFrame implements ChangeListener {
     /**
      * Returns the maze object
      * 
-     * @return The maze object
+     * @return maze the maze object
      */
     public Maze getMaze() {
         return maze;
@@ -29,7 +29,7 @@ public class GUI extends JFrame implements ChangeListener {
     /**
      * Returns the panel object
      * 
-     * @return The panel object
+     * @return panel the panel object
      */
     public Panel getPanel() {
         return panel;
